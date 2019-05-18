8b0bd2deff0958e3231a543cbb947bf46b5560c8
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-18 16:01:25 +0100

    adding auth in auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,13 +5,15 @@ const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
-// @access  PUBLIC
+// @access  PRIVATE
 router.get('/', auth, async (req, res) => {
     try {
-      const user = await User.findById()
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
     }
     catch(err){
-
+      console.error(err.message);
+      res.status(500).send('Server Error!');
     }
 });
 
